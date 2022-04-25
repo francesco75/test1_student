a447619e54cd3d79991532696892112a67ad5f7a
FRSCO <frscollo@hotmail.it>
2022-04-25 19:21:54 +0100

    Added index file and composer.json

--- includes/header.php
+++ includes/header.php
@@ -7,8 +7,10 @@
     <title>Test Student</title>
 
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+    <meta name="description" content="">
+    <meta name="author" content="">
     <!-- Bootstrap Core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
