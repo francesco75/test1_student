f027fe2727faba63340d1f9fa3c780a4976fa63b
FRSCO <frscollo@hotmail.it>
2022-06-09 18:13:10 +0100

    Added footer

--- xampp1/htdocs1/learn/admin/includes/footer.php
+++ xampp1/htdocs1/learn/admin/includes/footer.php
@@ -1,7 +1,7 @@
 <footer>
         <div class="py-3 text-center">
          <hr>
-         Copyright @ Francesco Scollo<br>
+         Copyright @ Web Developer Francesco Scollo<br>
          <a href="https://www.facebook.com/francesco.scollo1/" target="_blank"   class="fa fa-facebook" ></a>
         <a href="https://twitter.com/FrancescoScoll2" target="_blank" class="fa fa-twitter"></a>
         <a href="https://www.linkedin.com/in/francesco-scollo-9b69bb103/" target="_blank" class="fa fa-linkedin" ></a>
