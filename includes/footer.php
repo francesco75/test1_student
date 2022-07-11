1ab32e5418bc096d15d0cd9798eb29e426578435
FRSCO <frscollo@hotmail.it>
2022-07-11 17:27:46 +0100

    add test responsive

---
+++ includes/footer.php
@@ -0,0 +1,54 @@
+<footer>
+        <div class="py-3 text-center">
+         <hr>
+         Copyright @ Web Developer Francesco Scollo<br>
+        <a href="https://www.facebook.com/francesco.scollo1/" target="_blank"   class="fa fa-facebook" ></a>
+        <a href="https://twitter.com/FrancescoScoll2" target="_blank" class="fa fa-twitter"></a>
+        <a href="https://www.linkedin.com/in/francesco-scollo-9b69bb103/" target="_blank" class="fa fa-linkedin" ></a>
+        <a href="#" class="fa fa-youtube"></a>
+        <a href="https://www.instagram.com/francesco.scollo1/" target="_blank" class="fa fa-instagram"></a>
+        <a href="https://www.pinterest.co.uk/francesco48760821/_created/" target="_blank" class="fa fa-pinterest"></a>
+
+         </div> 
+     </footer>   
+ </body>
+ <script type="text/javascript">
+    google.charts.load("current", {packages:["corechart"]});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ["Subjects", "Marks", { role: "style" } ],
+        ["Algebra", <?php echo $student_found->va;?>, "gold"],
+        ["Geometry", <?php echo $student_found->vg;?>, "#ffffff"],
+        ["History", <?php echo $student_found->vh;?>, "gold"],
+        ["Geography",<?php echo $student_found->vgeo;?>, "color: #ffffff"],
+        ["Science", <?php echo $student_found->vs;?>, "gold"],
+        ["Art", <?php echo $student_found->vart;?>, "#ffffff"],
+        ["Music",<?php echo $student_found->vmus;?>, "gold"]
+      ]);
+
+      var view = new google.visualization.DataView(data);
+      view.setColumns([0, 1,
+                       { calc: "stringify",
+                         sourceColumn: 1,
+                         type: "string",
+                         role: "annotation" },
+                       2]);
+
+      var options = {
+        title: "Marks",
+        height: 400,
+        bar: {groupWidth: "98%"},
+        legend: { position: "none" },
+      };
+      var chart = new google.visualization.BarChart(document.getElementById("barchart_values"));
+      chart.draw(view, options);
+  }
+  $(window).resize(function(){
+        drawChart();
+    });
+
+  
+  </script>
+  
+</html>
---
+++ includes/header.php
@@ -0,0 +1,30 @@
+
+<?php require_once("xampp1/htdocs1/learn/admin/includes/init.php");?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Test Student</title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+     
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+     <link rel="stylesheet" href="css/style.css">
+     <link rel="stylesheet" href="css/responsive.css">
+     <link rel="stylesheet" href="css/responsive1.css">
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>   
+</head>
+
